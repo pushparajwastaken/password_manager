6ae0a2431ab132fa20329426f086fe59cf380dfe
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-11-24 22:44:38 +0530

    first day

---
+++ src/Components/PasswordSaver/PasswordSaver.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const PasswordSaver = () => {
+  return <div>Password Saver Component</div>;
+};
---
+++ src/Components/SignUp/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { LockOpen } from "lucide-react";
+const SignUp = () => {
+  return (
+    <div className="min-h-screen w-full bg-[#F7FFE0] font-mono">
+      <LockOpen className="text-[#896A58] " size={42} />
+      <form action="" className=" text-[#896A58]">
+        <h2 className="pb-2">SignUp Today</h2>
+      </form>
+    </div>
+  );
+};
+export default SignUp;
