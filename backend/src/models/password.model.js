be67f988f4254e1efa2bcbb113d6243f6561d9c8
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-01-16 13:41:46 +0530

    add password model

---
+++ backend/src/models/password.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const passwordSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    encryptedPassword: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    website: {
+      type: String,
+    },
+    iv: {
+      type: String,
+      required: true,
+    },
+    authTag: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Password = mongoose.model("Password", passwordSchema);
