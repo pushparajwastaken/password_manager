5dcb0735ad5e2d899b6ecc898c0bde806ba65043
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-01-21 10:41:27 +0530

    add password controller

--- backend/src/controllers/password.controller.js
+++ backend/src/controllers/password.controller.js
@@ -4,6 +4,8 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import { deriveKey, encrypt, decrypt } from "../utils/crypto.utils.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+import { isValidObjectId } from "mongoose";
 export const CreatePassword = asyncHandler(async (req, res) => {
   const { password, title, website, masterPassword } = req.body;
   if ([password, title, masterPassword].some((f) => !f?.trim())) {
@@ -32,17 +34,75 @@ export const CreatePassword = asyncHandler(async (req, res) => {
     iv,
     authTag,
   });
+  return res.status(201).json(
+    new ApiResponse(
+      201,
+      {
+        _id: savedPassword._id,
+        title,
+        website,
+        createdAt: savedPassword.createdAt,
+      },
+      "Password saved successfully",
+    ),
+  );
+});
+export const updatePassword = asyncHandler(async (req, res) => {
+  const { passwordId, newPassword, masterPassword, title, website } = req.body;
+  if (isValidObjectId(passwordId)) {
+    throw new ApiError(400, "Invalid Password Id");
+  }
+  const password = await Password.findById(passwordId);
+  if (!password) {
+    throw new ApiError(404, "Password not found");
+  }
+  if (!password.owner.equals(req.user._id)) {
+    throw new ApiError(403, "Unauthorized to delete the password");
+  }
+
+  const user = await User.findById(req.user._id).select(
+    "+masterPassword +salt",
+  );
+  const isValid = await user.isPasswordCorrect(masterPassword);
+  if (!isValid) {
+    throw new ApiError(401, "Invalid master password");
+  }
+
+  const key = deriveKey(masterPassword, user.salt);
+  const { cipherText, iv, authTag } = encrypt(newPassword, key);
+
+  password.encryptedPassword = cipherText;
+  password.iv = iv;
+  password.authTag = authTag;
+
+  await password.save();
+
   return res
     .status(200)
-    .json(new ApiResponse(200, savedPassword, "Password Saved Successfully"));
+    .json(new ApiResponse(200, {}, "Password updated successfully"));
 });
-export const updatePassword = asyncHandler(async (req, res) => {});
-export const deletePassword = asyncHandler(async (req, res) => {});
+export const deletePassword = asyncHandler(async (req, res) => {
+  const { passwordId } = req.body;
+  if (!isValidObjectId(passwordId)) {
+    throw new ApiError(400, "Invalid Password Id");
+  }
+  const password = await Password.findById(passwordId);
+  if (!password) {
+    throw new ApiError(404, "Password not found");
+  }
+  if (!password.owner.equals(req.user._id)) {
+    throw new ApiError(403, "Unauthorized to delete the password");
+  }
+  await password.deleteOne();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password deleted successfully"));
+});
 export const getAllUserPasswords = asyncHandler(async (req, res) => {
   const passwords = await Password.find({
     owner: req.user._id,
   }).select("title website createdAt updatedAt");
-  if (!passwords) {
+  if (passwords.length === 0) {
     throw new ApiError(404, "No passwords found");
   }
   return res
