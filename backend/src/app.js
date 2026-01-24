6705963bfb73595e0e6797f04c55c9cd4ecfef08
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-01-24 21:28:43 +0530

    connect frontend to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,11 +11,12 @@ app.use(
     credentials: true,
   }),
 );
+app.use(helmet());
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(helmet());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/password", passwordRouter);
 export { app };
