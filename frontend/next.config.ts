f78bee09486ae43654a2aeeef13df6f744c08a6d
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-03-28 17:38:23 +0530

    add pwa configurations

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // disable in dev
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
